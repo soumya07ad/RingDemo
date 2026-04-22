15ec322a3fb838134f8c1ce7a5af6780ba74648e
soumya07ad <adhikarisoumya7@gmail.com>
2026-04-22 15:54:50 +0530

    fix: Add missing BorderStroke import in PremiumComponents.kt

--- src/main/kotlin/com/dkgs/innerpulse/ui/components/PremiumComponents.kt
+++ src/main/kotlin/com/dkgs/innerpulse/ui/components/PremiumComponents.kt
@@ -2,6 +2,7 @@ package com.dkgs.innerpulse.ui.components
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
